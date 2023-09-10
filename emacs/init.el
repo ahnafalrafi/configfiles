64e24ccdea219f076dcfc925dc283d7cc77a7fed
Ahnaf Rafi <ahnaf.al.rafi@gmail.com>
2023-09-10 14:36:50 -0500

    Clean up init.el, add package management.

--- emacs/init.el
+++ emacs/init.el
@@ -2,13 +2,25 @@
 
 ;;; Code:
 
+;; Name and email.
 (setq user-full-name "Ahnaf Rafi")
 (setq user-mail-address "ahnaf.al.rafi@gmail.com")
 
-;; Set file for custom.el to use
+;; Set file for custom.el to use --- I use this for temporary customizations.
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-
+;; Load the custom file.
 (load custom-file 'noerror 'nomessage)
 
-(provide 'init)
-;;; init.el ends here
+;; Add user lisp directory to load path.
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(setq use-package-always-ensure t)
